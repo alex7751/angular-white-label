063a3bb595b638e671d33b0ce1ad48dbe74c2d85
Alex Ferguson <alex.ferguson@soprasteria.com>
2018-03-01 21:33:36 +0000

    Core module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,60 +14,24 @@ import { CustomRouterStateSerializer } from './shared/utils';
 import { RouterModule } from '@angular/router';
 import { routes } from './routes';
 
-/**
- * Common modules associated with the Angular framework
- */
 const modules = [
   CommonModule,
   BrowserModule,
   BrowserAnimationsModule,
   HttpClientModule,
-  RouterModule.forRoot(routes, {useHash: true})
+  RouterModule.forRoot(routes, { useHash: true })
 ];
 
-/**
- * Modules/configurations associated with ngrx library
- */
 const ngrxModules = [
-  /**
-   * StoreModule.forRoot is imported once in the root module, accepting a reducer
-   * function or object map of reducer functions. If passed an object of
-   * reducers, combineReducers will be run creating your application
-   * meta-reducer. This returns all providers for an @ngrx/store
-   * based application.
-   */
-  StoreModule.forRoot(reducers, {metaReducers}),
-  /**
-   * Use the dev tools if not production. Install Redux DevTools Chrome extension.
-   */
+  StoreModule.forRoot(reducers, { metaReducers }),
   !environment.production ? StoreDevtoolsModule.instrument() : [],
-  /**
-   * EffectsModule.forRoot() is imported once in the root module and
-   * sets up the effects class to be initialized immediately when the
-   * application starts.
-   *
-   * See: https://github.com/ngrx/platform/blob/master/docs/effects/api.md#forroot
-   */
-  EffectsModule.forRoot([AppEffects]),
-  /**
-   * The stateKey defines the name of the state used by the router-store reducer.
-   * This matches the key defined in the map of reducers
-   */
+  EffectsModule.forRoot([]),
   StoreRouterConnectingModule.forRoot({
     stateKey: 'router',
   }),
 ];
-
-/**
- * App providers
- */
 const providers = [
-  /**
-   * The `RouterStateSnapshot` provided by the `Router` is a large complex structure.
-   * A custom RouterStateSerializer is used to parse the `RouterStateSnapshot` provided
-   * by `@ngrx/router-store` to include only the desired pieces of the snapshot.
-   */
-  {provide: RouterStateSerializer, useClass: CustomRouterStateSerializer}
+  { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer }
 ];
 
 @NgModule({
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,24 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze'
+import * as fromRouter from '@ngrx/router-store';
+import { RouterStateUrl } from '../shared/utils';
 
 export interface State {
-
+  router: fromRouter.RouterReducerState<RouterStateUrl>
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  router: fromRouter.routerReducer
 };
 
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return function (state: State, action: any): State {
+    console.log('state', state);
+    console.log('action', action);
+    return reducer(state, action);
+  };
+}
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
